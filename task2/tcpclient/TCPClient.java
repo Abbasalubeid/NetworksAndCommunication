6d6ad95b9779db8988624c896bd18ef142d7b818
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-02-15 22:13:39 +0100

    Pasted the code from task1

--- task2/tcpclient/TCPClient.java
+++ task2/tcpclient/TCPClient.java
@@ -1,13 +1,64 @@
 package tcpclient;
 import java.net.*;
+
 import java.io.*;
 
 public class TCPClient {
+    private static int BUFFERSIZE = 1024;
     
     public TCPClient(boolean shutdown, Integer timeout, Integer limit) {
     }
 
     public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
-        return new byte[0];
+
+        //Byte array that grows dynamically since we do not know how much data we receive
+        ByteArrayOutputStream fromServerBuffer = new ByteArrayOutputStream();
+        //Temporarily storage for each "read" iteration of data
+        byte[] intermediateStorage = new byte[BUFFERSIZE];
+
+        //This constructor calls connect to the given hostname at the given port 
+        Socket clientSocket = new Socket(hostname, port);
+        clientSocket.getOutputStream().write(toServerBytes); //send(...)
+        //Get the input stream from the socket to perform recv(...) later
+        InputStream input = clientSocket.getInputStream(); 
+        
+        receive(intermediateStorage, fromServerBuffer, input);
+
+        clientSocket.close();
+
+        return fromServerBuffer.toByteArray();
     }
+
+    public byte[] askServer(String hostname, int port) throws IOException {
+        //Byte array that grows dynamically since we do not know how much data we receive
+        ByteArrayOutputStream fromServerBuffer = new ByteArrayOutputStream();
+        //Temporarily storage for each "read" iteration of data
+        byte[] intermediateStorage = new byte[BUFFERSIZE];
+
+        //This constructor calls connect to the given hostname at the given port 
+        Socket clientSocket = new Socket(hostname, port);
+        //Get the input stream from the socket to perform recv(...) later
+        InputStream input = clientSocket.getInputStream(); 
+        
+        receive(intermediateStorage, fromServerBuffer, input);
+
+        clientSocket.close();
+
+        return fromServerBuffer.toByteArray();
+    }
+
+    private void receive(byte[] intermediate, ByteArrayOutputStream buffer, InputStream input)throws IOException{
+        //Temporary saves the returned "read amount" for each read iteration
+        int currentLength;
+        while(true){
+            //recv(...)
+            currentLength = input.read(intermediate);
+            //-1 indicates end of the stream, no more data to collect
+            if(currentLength == -1)
+                break;
+            buffer.write(intermediate, 0, currentLength);
+        }
+    }
+
+
 }
