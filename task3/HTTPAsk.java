80b706a4d945e8f125eb495f376a6c14a45e52d5
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-02-25 20:51:41 +0100

    Simple http response done

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -2,8 +2,42 @@ import java.net.*;
 import java.io.*;
 
 public class HTTPAsk {
-    public static void main( String[] args) {
-        // Your code here
+
+    public static void main(String[] args) {
+        try {
+            // create a ServerSocket on port 8080
+            ServerSocket serverSocket = new ServerSocket(8080);
+            
+            // continuously listen for incoming connections
+            while (true) {
+                // accept the incoming client connection
+                Socket clientSocket = serverSocket.accept();
+                
+                // handle the incoming client request
+                handleRequest(clientSocket);
+            }
+        } catch (IOException e) {
+            // print the error message in case of exception
+            System.err.println("IOException: " + e.getMessage());
+        }
+    }
+
+    private static void handleRequest(Socket clientSocket) throws IOException {
+        // construct the HTTP response header
+        String response = "HTTP/1.1 200 OK\r\n" +
+                          "Content-Type: text/plain\r\n" +
+                          "\r\n" +
+                          "This is from HTTPAsk";
+        byte[] responseBytes = response.getBytes("UTF-8");
+    
+        // get the output stream of the client socket
+        OutputStream outputStream = clientSocket.getOutputStream();
+        
+        // write the response bytes to the output stream and flush the stream
+        outputStream.write(responseBytes);
+        outputStream.flush();
+    
+        // close the client socket
+        clientSocket.close();
     }
 }
-
